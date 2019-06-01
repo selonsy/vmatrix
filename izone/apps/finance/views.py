d49fdaba05ac1f23baec48be9e1f139bbecc423b
shenjl <selonsy@gmail.com>
2019-06-01 23:35:06 +0800

    update.

--- izone/apps/finance/views.py
+++ izone/apps/finance/views.py
@@ -1,6 +1,6 @@
-# from django.shortcuts import render
-# from django.http import JsonResponse
-# from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
 from .service import *
 
 
